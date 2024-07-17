2e3e460edd31e4f34c8bb3426e7bdf0678e21e42
David <nhanitk14dev@gmail.com>
2024-07-18 01:07:08 +0700

    Task 1 - create get endpoint to retrieve review data

---
+++ app/Http/Controllers/ReviewController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\CarDrivingService;
+
+class ReviewController extends Controller
+{
+    protected $carDrivingService;
+
+    public function __construct(CarDrivingService $carDrivingService)
+    {
+        $this->carDrivingService = $carDrivingService;
+    }
+
+    public function getReview($capId)
+    {
+        try {
+            $response = $this->carDrivingService->getReview($capId);
+            return response()->json($response);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Unable to fetch review data', 'message' => $e->getMessage()], 500);
+        }
+    }
+}
